02694066d64cfaad572d59a040d07b1f5e7d0eea
Aswin Raj Kharel <query.ark@gmail.com>
2020-10-15 08:51:02 -0500

    Create Multipart.h

---
+++ Multipart.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <memory>
+
+namespace HTTP {
+	class Multipart {
+	private:
+		std::string _boundary;
+		std::stringstream _body;
+	public:
+		Multipart(std::string const& boundary);
+		void addTextParameter(std::string const& name, std::string const& value);
+		void addFileParameter(std::string const& name, std::string const& filepath, std::string const& filename, std::string const& contentType);
+		void addRawImage(std::string const& name, unsigned char* data, size_t size, std::string const& filename, std::string const& contentType);
+		std::string getBody();
+		std::string getContentType();
+	};
+}
